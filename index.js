69d0588b11a0c7a9e99225435a376528f6dc233b
Noah Gels <minehd.hdmine@gmail.com>
2021-06-22 15:49:57 +0200

    added comments to index

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 const app = express()
-
 const PORT = 8080   
 
+// create a get request, this also wo
 app.get('/', (req, res) => {                      
-    res.end('This is a get request')
+    res.end('Welcome to the Server')
 })
 
+// log the port to keep the overview
 app.listen(PORT, () => {                
     console.log(`Running on port ${PORT}`)
 })
